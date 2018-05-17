a74bed5b6a13ae5a05e010fbf03818ea229d6f77
shubhs2060 <shubhamsrivastava2050>
2018-05-17 14:32:38 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+1. Do bundle install to install the gem
+
+2. Set up the db
+
+   Create the db
+   rails db:create
+
+   Migrate the db
+   rails db:migrate
+
+3. Generate sample data
+
+   rails db:seed
+   By default I have stored the cost of product in Dollars
+
+4. Start the rails server
+
+   rails s
+
+5. Navigate to http://localhost:3000
+
+   Here you will see the list of products 
+
+   To get the cost of product in your region
+
+   Click the link "Check Rate For Your Country"
+
+   This will show you the rate of the product in your country
+
+   You can login from any country and check the cost of product it works with all
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'Item 1', description: "Inventory Item", cost: "100")
+Product.create(name: 'Item 2', description: "Inventory Item", cost: "200")
+Product.create(name: 'Item 3', description: "Inventory Item", cost: "300")
+Product.create(name: 'Item 4', description: "Inventory Item", cost: "400")
+Product.create(name: 'Item 5', description: "Inventory Item", cost: "500")
+Product.create(name: 'Item 6', description: "Inventory Item", cost: "600")
